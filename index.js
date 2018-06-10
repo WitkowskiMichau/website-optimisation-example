d329a1e0bbff270c7705059a08ae6eb339ffa4a2
Michal Witkowski <batoponton@gmail.com>
2018-06-10 20:12:44 +0200

    Added compression

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const compression = require('compression')
+// This require installing compression via npm - look in commit
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'))
+
+app.use(compression())
+// Added compression with GZIP
 
 
 app.listen(PORT, () => console.log(`Helo!! Port: ${PORT}`))
